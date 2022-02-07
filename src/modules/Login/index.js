bebdd54e9687cb1a3a99fe83a61f32424d55bf4e
Renjith <renjith.gencue@outlook.com>
2022-02-07 05:27:58 -0800

    added the login

--- src/modules/Login/index.js
+++ src/modules/Login/index.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import SocialButton from '../../components/SocialButton'
 
 
 import { handleDecrementClick, handleIncrementClick } from './actions';
 
 import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
+import './style.css';
+import { faSortNumericUpAlt } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faGoogle, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
 export class Login extends React.PureComponent {
     constructor(props) {
         super(props);
@@ -26,73 +32,123 @@ export class Login extends React.PureComponent {
         e.preventDefault();
         //if username or password field is empty, return error message
         // if (this.state.username === "" || this.state.password === "") {
-            // this.setState({ errorMessage: "Empty username/password field" })
+        // this.setState({ errorMessage: "Empty username/password field" })
         // } else if (this.state.username == "admin" && this.state.password == "123456") {
-            //Signin Success
-            localStorage.setItem("isAuthenticated", "true");
-            window.location.pathname = "/";
+        //Signin Success
+        localStorage.setItem("isAuthenticated", "true");
+        window.location.pathname = "/";
         // } else {
-            // this.setState({ errorMessage: "Invalid username/password" })
+        // this.setState({ errorMessage: "Invalid username/password" })
 
         // }
     }
+    handleSocialLogin = (user) => {
+        console.log(user);
+        localStorage.setItem("userLogin", JSON.stringify(user._profile));
 
+      };
+      
+    handleSocialLoginFailure = (err) => {
+        console.error(err);
+      };
 
     render() {
         console.log(this.props.count)
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6">
-                        <h1>Hello world Redux Saga! {this.props.count}</h1>
-                        <Button onClick={this.props.handleIncrementClick} variant="outline-success">Increment</Button>{' '}
 
-                        <Button onClick={this.props.handleDecrementClick} variant="outline-danger">Decrement</Button>{' '}
+
+            <section className="vh-100">
+                <div className="container-fluid h-custom">
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                        <div className="col-md-9 col-lg-6 col-xl-5">
+                            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" className="img-fluid" alt="Sample image" />
+                        </div>
+                        <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                            <form>
+                                <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+                                    <p className="lead fw-normal mb-0 me-3">Sign in with</p>
+
+                                    <SocialButton variant="primary" className="btn btn-default btn-circle mx-1"
+                                     scope="public_profile,email"
+                                    provider="facebook"
+                                    appId="1817456088401252"
+                                    onLoginSuccess={this.handleSocialLogin}
+                                    onLoginFailure={this.handleSocialLoginFailure}
+                                    >
+                                        <FontAwesomeIcon icon={faFacebook} />
+                                    </SocialButton>
+                                    <Button variant="primary" className="btn btn-default btn-circle mx-1">
+                                        <FontAwesomeIcon icon={faTwitter} />
+                                    </Button>
+                                    <Button variant="primary" className="btn btn-default btn-circle mx-1">
+                                        <FontAwesomeIcon icon={faLinkedin} />
+                                    </Button>
+
+                                </div>
+
+                                <div className="divider d-flex align-items-center my-4">
+                                    <p className="text-center fw-bold mx-3 mb-0">Or</p>
+                                </div>
+
+
+                                <div className="form-outline mb-4">
+                                    <input type="email" id="form3Example3"
+                                        onChange={(e) => this.setState({ username: e })} className="form-control form-control-lg"
+                                        placeholder="Enter a valid email address" />
+                                    <label className="form-label" htmlFor="form3Example3">Email address</label>
+                                </div>
+
+
+                                <div className="form-outline mb-3">
+                                    <input type="password" id="form3Example4" className="form-control form-control-lg"
+                                        placeholder="Enter password" onChange={(e) => this.setState({ username: e })} />
+                                    <label className="form-label" htmlFor="form3Example4">Password</label>
+                                </div>
+
+                                <div className="d-flex justify-content-between align-items-center">
+
+                                    <div className="form-check mb-0">
+                                        <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+                                        <label className="form-check-label" htmlFor="form2Example3">
+                                            Remember me
+                                        </label>
+                                    </div>
+                                    <a href="#!" className="text-body">Forgot password?</a>
+                                </div>
+
+                                <div className="text-center text-lg-start mt-4 pt-2">
+                                    <Button variant="primary" className="btn btn-primary btn-lg"
+                                        style={{ paddingLeft: '2.5rem', paddingRight: '2.5rem' }} onClick={this.handleSubmit}>Login</Button>{' '}
+                                    <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
+                                        className="link-danger">Register</a></p>
+                                </div>
+
+                            </form>
+                        </div>
                     </div>
                 </div>
-                <div className="text-center">
-                    <h1>Signin User</h1>
-                    <form
-                        style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                    >
-                        <div className="form-group">
-                            <label>Username</label>
-                            <input
-                                className="form-control"
-                                type="text"
-                                name="username"
-                                onChange={(e) => this.setState({ username: e })}
-                            />
-                        </div>
+                <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
 
-                        <div className="form-group">
-                            <label>Password</label>
-                            <input
-                                className="form-control"
-                                type="password"
-                                name="password"
-                                onChange={(e) => this.setState({ username: e })}
-                            />
-                        </div>
-                        <button
-                            type="submit"
-                            className="btn btn-primary"
-                            onClick={this.handleSubmit}
-                        >
-                            Submit
-                        </button>
-                        {this.state.errorMessage && (
-                            <p className="text-danger"> {this.state.errorMessage} </p>
-                        )}
+                    <div className="text-white mb-3 mb-md-0">
+                        Copyright © 2020. All rights reserved.
+                    </div>
+                    <div>
+                        <a href="#!" className="text-white me-4">
+                            <FontAwesomeIcon icon={faFacebook} />
+                        </a>
+                        <a href="#!" className="text-white me-4">
+                            <FontAwesomeIcon icon={faTwitter} />
+                        </a>
+                        <a href="#!" className="text-white me-4">
+                            <FontAwesomeIcon icon={faGoogle} />
+                        </a>
+                        <a href="#!" className="text-white">
+                            <FontAwesomeIcon icon={faLinkedin} />
+                        </a>
+                    </div>
 
-                    </form>
                 </div>
-            </div>
+            </section>
         );
     }
 }
